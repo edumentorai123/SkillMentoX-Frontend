315af431cdc3e5a103a9fc2fc5fc4865304e1a31
iamfaisalpk <iamfaizy123@gmail.com>
2026-03-17 15:17:44 +0530

    chanegs needed

--- app/Student/Sidebar/Sidebar.tsx
+++ app/Student/Sidebar/Sidebar.tsx
@@ -19,6 +19,7 @@ function Sidebar() {
   const [isMobileOpen, setIsMobileOpen] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
 
+  
   useEffect(() => {
     const checkMobile = () => {
       setIsMobile(window.innerWidth < 768);
